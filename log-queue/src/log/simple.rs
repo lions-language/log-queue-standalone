07a4a3840b628866f54e07de5f3164cf48548335
lions-language <lions_language@163.com>
2021-05-03 10:42:48 +0800

    add load

--- log-queue/src/log/simple.rs
+++ log-queue/src/log/simple.rs
@@ -1,9 +1,26 @@
+use std::fs;
+
 struct SimpleLog {
+    dir: String
 }
 
 impl SimpleLog {
-    pub fn new() -> Self {
+    fn load(&mut self) {
+        let dirs = match fs::read_dir(&self.dir) {
+            Ok(d) => {
+                d
+            },
+            Err(err) => {
+                unreachable!("{:?}", err);
+            }
+        };
+        for dir in dirs.iter() {
+        }
+    }
+
+    pub fn new(dir: String) -> Self {
         Self {
+            dir: dir
         }
     }
 }
